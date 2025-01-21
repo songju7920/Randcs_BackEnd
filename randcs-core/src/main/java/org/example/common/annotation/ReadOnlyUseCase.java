e9571d32d2eea2991930a40fddc906a1fd39db84
오송주 <dhthdwn7920@gmail.com>
2025-01-21 21:41:58 +0900

    feat: 어노테이션들 생성

---
+++ randcs-core/src/main/java/org/example/common/annotation/ReadOnlyUseCase.java
@@ -0,0 +1,9 @@
+package org.example.common.annotation;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public @interface ReadOnlyUseCase {
+}
---
+++ randcs-presentation/src/main/java/org/example/common/annotation/WebAdapter.java
@@ -0,0 +1,7 @@
+package org.example.common.annotation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public @interface WebAdapter {
+}
