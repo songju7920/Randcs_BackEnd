ce4e1b376a08ea3f9f3eea1b2e44674f880b6599
오송주 <dhthdwn7920@gmail.com>
2025-01-21 21:43:59 +0900

    feat: CustomUserDetailsService에 user 검색 로직 추가

--- randcs-infrastructure/src/main/java/org/example/global/security/auth/CustomUserDetailsService.java
+++ randcs-infrastructure/src/main/java/org/example/global/security/auth/CustomUserDetailsService.java
@@ -1,10 +1,21 @@
 package org.example.global.security.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.example.domain.user.excetpion.UserNotFoundException;
+import org.example.domain.user.model.User;
+import org.example.domain.user.spi.QueryUserPort;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService {
+    private final QueryUserPort queryUserPort;
+
     public CustomUserDetails loadUserByEmail(String email) {
-        return null; // todo: userEntity mapping 완료되면 여기에 관련 로직 추가
+        User user = queryUserPort.getUserByEmail(email).orElseThrow(
+                () -> UserNotFoundException.EXCEPTION
+        );
+
+        return new CustomUserDetails(user.getEmail());
     }
 }
