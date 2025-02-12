019f9d139fb8fec3d31f1a225fae6b69f29c26ad
오송주 <dhthdwn7920@gmail.com>
2025-02-12 16:34:50 +0900

    fix: getUserProfileResponseDto 수정

--- randcs-core/src/main/java/org/example/domain/user/dto/response/GetUserProfileResponseDto.java
+++ randcs-core/src/main/java/org/example/domain/user/dto/response/GetUserProfileResponseDto.java
@@ -1,8 +1,25 @@
 package org.example.domain.user.dto.response;
 
-import org.example.domain.user.dto.vo.UserDataProfileVO;
+import org.example.domain.user.model.User;
+
+import java.math.BigInteger;
 
 public record GetUserProfileResponseDto(
-        UserDataProfileVO data
+        String nickname,
+        String profile,
+        String job,
+        String office,
+        String url,
+        BigInteger exp
 ) {
+    public static GetUserProfileResponseDto from(User user) {
+        return new GetUserProfileResponseDto(
+                user.getNickname(),
+                user.getProfile(),
+                user.getJob().orElse(null),
+                user.getOffice().orElse(null),
+                user.getUrl().orElse(null),
+                user.getExp()
+        );
+    }
 }
