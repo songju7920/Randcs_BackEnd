05129f38233fccb54820fefb825ad00055942934
오송주 <dhthdwn7920@gmail.com>
2025-02-13 03:24:48 +0900

    feat: custom enum validator 추가

---
+++ randcs-core/src/main/java/org/example/common/util/EnumValidator.java
@@ -0,0 +1,17 @@
+package org.example.common.util;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.example.common.annotation.ValidEnum;
+
+public class EnumValidator implements ConstraintValidator<ValidEnum, Enum> {
+    private ValidEnum annotation;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) { this.annotation = constraintAnnotation; }
+
+    @Override
+    public boolean isValid(Enum value, ConstraintValidatorContext constraintValidatorContext) {
+        return value != null;
+    }
+}
--- randcs-infrastructure/src/main/java/org/example/global/exception/GlobalExceptionHandler.java
+++ randcs-infrastructure/src/main/java/org/example/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package org.example.global.exception;
 
 import org.example.common.error.GlobalErrorCode;
+import org.example.common.error.exception.errorCode.CommonErrorCode;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.example.common.error.RandcsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +20,15 @@ public class GlobalExceptionHandler {
         ExceptionResponse response = ExceptionResponse.of(errorCode, errorCode.getErrorMessage());
 
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(errorCode.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ExceptionResponse> validationExceptionHandler(MethodArgumentNotValidException e) {
+        String description = e.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> fieldError.getField() + " : " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(" / "));
+        ExceptionResponse response = ExceptionResponse.of(CommonErrorCode.BAD_REQUEST, description);
+
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(400));
     }
 }
